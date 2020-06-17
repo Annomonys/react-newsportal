084f7cc1248c77f61d7763bc8b418ee58d0d3501
Suraj Libi <suraj@qodemedia.com>
2020-06-18 01:04:55 +0545

    Updates

---
+++ precache-manifest.ff8d6353bf822c26a954c7f2d4667b82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ad594a9827f7896cd3a1e43092d6fc",
+    "url": "/react-newsportal/index.html"
+  },
+  {
+    "revision": "45441f5c87fd9b645a7c",
+    "url": "/react-newsportal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50f260645be36807fc47",
+    "url": "/react-newsportal/static/js/2.d3b9cb94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-newsportal/static/js/2.d3b9cb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45441f5c87fd9b645a7c",
+    "url": "/react-newsportal/static/js/main.a22910b3.chunk.js"
+  },
+  {
+    "revision": "ef7de8c198ee0172215b",
+    "url": "/react-newsportal/static/js/runtime-main.aab513ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-newsportal/static/media/logo.5d5d9eef.svg"
+  }
+]);
